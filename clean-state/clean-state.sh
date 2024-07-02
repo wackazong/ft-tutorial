fe9bed0e08aebbbf3b0dd8deb359a840380a3241
wackazong <alexander@wackazong.com>
2024-07-02 13:05:58 +0000

    feat: check env variables for clean-state

--- clean-state/clean-state.sh
+++ clean-state/clean-state.sh
@@ -10,9 +10,15 @@ if [ "$1" != "testnet" ] && [ "$1" != "mainnet" ]; then
     echo "Error: Invalid parameter '$1'. Acceptables values: testnet or mainnet"
 fi
 
+# Check if FT_CONTRACT_ID is set
+if [ -z "$FT_CONTRACT_ID" ]; then
+  echo "Error: Environment variable FT_CONTRACT_ID is not set."
+  exit 1
+fi
+
 # get the storage keys
 KEYS=$(curl -sX POST https://rpc."$1".near.org -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","id":"dontcare","method":"query","params":{"request_type":"view_state","finality":"final","account_id":"'"$FT_CONTRACT_ID"'","prefix_base64":""}}' | jq -c '.result.values | map([.key]) | flatten')
 KEYS='{"keys":'$KEYS'}'
 
 # call the clean method of the contract
-near contract call-function as-transaction "$FT_CONTRACT_ID" clean base64-args $(echo $KEYS | base64) prepaid-gas '100.0 Tgas' attached-deposit '0 NEAR' sign-as "$FT_CONTRACT_ID" network-config "$1" sign-with-legacy-keychain
+near contract call-function as-transaction "$FT_CONTRACT_ID" clean base64-args $(echo $KEYS | base64 -w 0) prepaid-gas '100.0 Tgas' attached-deposit '0 NEAR' sign-as "$FT_CONTRACT_ID" network-config "$1" sign-with-legacy-keychain send
